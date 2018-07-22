4ad182dad9a7a5ed9ac58530ffadffc2117efcc9
aiokr <hiaiokr@gmail.com>
2018-07-22 16:10:42 +0800

    update ci

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+cd ./public
+git init
+git config --global push.default matching
+git config --global user.email "${GitHubEMail}"
+git config --global user.name "${GitHubUser}"
+git add --all .
+git commit -m "Auto Builder of ${GitHubUser}'s blog"
+git push --force "https://${GitHubKEY}@github.com/${GitHubUser}/hexo-page.git" master
